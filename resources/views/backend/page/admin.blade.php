da7e645fe1114eff915f19bf8c57449710dd9638
trisha <trisha123nath321@gmail.com>
2022-12-26 00:30:01 -0800

    create admin

--- resources/views/backend/page/admin.blade.php
+++ resources/views/backend/page/admin.blade.php
@@ -12,24 +12,15 @@
     </tr>
   </thead>
   <tbody>
+    @foreach ($mainpage as $singlemainpage)
     <tr>
-      <th scope="row">1</th>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
+      <th scope="row">{{$singlemainpage->id}}</th>
+      <td>{{$singlemainpage->Email}}</td>
+      <td>{{$singlemainpage->Password}}</td>
+     
     </tr>
-    <tr>
-      <th scope="row">2</th>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
-    </tr>
-    <tr>
-      <th scope="row">3</th>
-      <td>Larry</td>
-      <td>the Bird</td>
-      <td>@twitter</td>
-    </tr>
+    
+    @endforeach
   </tbody>
 </table>
 @endsection
